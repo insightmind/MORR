a87cdce47db6541a0d81ef94ceb4b39cccc33a42
Sönke <uynfo@student.kit.edu>
2019-11-29 10:33:29 +0100

    Add base event type and examples

---
+++ application/Modules/Mouse/Events/MouseMoveEvent.cs
@@ -0,0 +1,16 @@
+﻿using System.Numerics;
+using MORR.Shared;
+
+namespace MORR.Modules.Mouse.Events
+{
+    /// <summary>
+    ///     A mouse move user interaction
+    /// </summary>
+    public class MouseMoveEvent : Event
+    {
+        /// <summary>
+        ///     The movement of the mouse
+        /// </summary>
+        public Vector2 Movement { get; set; }
+    }
+}
