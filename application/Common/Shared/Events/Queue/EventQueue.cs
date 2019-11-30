a68a13d1355acdef72af7ecb850cd9a13e7c3a65
Sönke <uynfo@student.kit.edu>
2019-11-30 21:36:59 +0100

    Added experimental event queue interfaces

---
+++ application/Common/Shared/Events/Queue/EventQueue.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace MORR.Shared.Events.Queue
+{
+    /// <summary>
+    ///     Provides a single-writer-multiple-reader queue for <see cref="Event" /> types
+    /// </summary>
+    /// <typeparam name="T">The type of the event</typeparam>
+    public abstract class EventQueue<T> : ITypedEventQueue<T>, IReadOnlyUntypedEventQueue where T : Event
+    {
+        /// <summary>
+        ///     Asynchronously gets all events as common type <see cref="Event" />
+        /// </summary>
+        /// <returns>A stream of <see cref="Event" /></returns>
+        public abstract IAsyncEnumerable<Event> GetUntypedEvents();
+
+        /// <summary>
+        ///     Asynchronously gets all events as concrete type <typeparamref name="T" />
+        /// </summary>
+        /// <returns>A stream of <typeparamref name="T" /></returns>
+        public abstract IAsyncEnumerable<T> GetTypedEvents();
+
+        /// <summary>
+        ///     Asynchronously enqueues a new event
+        /// </summary>
+        /// <param name="event">The event to enqueue</param>
+        public abstract void Enqueue(T @event);
+    }
+}
---
+++ application/Common/Shared/Events/Queue/ITypedEventQueue.cs
@@ -0,0 +1,15 @@
+﻿namespace MORR.Shared.Events.Queue
+{
+    /// <summary>
+    ///     Provides read-write access to a queue of events as concrete type <typeparamref name="T" />
+    /// </summary>
+    /// <typeparam name="T">The concrete type of the event</typeparam>
+    public interface ITypedEventQueue<T> : IReadOnlyTypedEventQueue<T> where T : Event
+    {
+        /// <summary>
+        ///     Asynchronously enqueues a new event
+        /// </summary>
+        /// <param name="event">The event to enqueue</param>
+        void Enqueue(T @event);
+    }
+}
