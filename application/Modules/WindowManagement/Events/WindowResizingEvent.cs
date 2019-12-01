3b6e715af646b78c532c47e3026c5d674c6c3a41
Li <limingyialpha@gmail.com>
2019-12-01 16:37:07 +0100

    Add WindowResizingEvent to specify the window resizing event

---
+++ application/Modules/WindowManagement/Events/WindowResizingEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+
+namespace MORR.Modules.Mouse.Events
+{
+    /// <summary>
+    ///     A window resizing user interaction
+    /// </summary>
+    public class WindowResizingEvent : WindowEvent
+    {
+        /// <summary>
+        ///     The old size of the window
+        /// </summary>
+        public Size OldSize { get; set; }
+
+        /// <summary>
+        ///     The new size of the window
+        /// </summary>
+        public Size NewSize { get; set; }
+    }
+}
