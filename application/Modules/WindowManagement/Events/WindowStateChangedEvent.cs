bb5a236a7f6767001fe8b6aad43fc71e7cbf715d
Li <limingyialpha@gmail.com>
2019-12-01 16:38:39 +0100

    Add class WindowStateChangedEvent to specify the window state changed event Include PresentationFramework in the Dependencies.Assembly

---
+++ application/Modules/WindowManagement/Events/WindowStateChangedEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace MORR.Modules.Mouse.Events
+{
+    /// <summary>
+    ///     An user interaction that changes the state of a window
+    /// </summary>
+    public class WindowStateChangedEvent: WindowEvent
+    {
+        /// <summary>
+        /// The new State of the window
+        /// </summary>
+        public WindowState NewState;
+    }
+}
