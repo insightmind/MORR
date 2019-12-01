22b4a924169214e3dbced7b6a957987aa9bc4391
Li <limingyialpha@gmail.com>
2019-12-01 12:20:45 +0100

    Add abstract class ClipboardEvent

---
+++ application/Modules/Clipboard/Events/ClipboardEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MORR.Shared;
+
+namespace MORR.Modules.Clipboard.Events
+{
+    /// <summary>
+    /// A generic clipboard event which all specific ClipboardEvents inherit from.
+    /// </summary>
+    public abstract class ClipboardEvent : Event
+    {
+        // TODO: Specify
+    }
+}
