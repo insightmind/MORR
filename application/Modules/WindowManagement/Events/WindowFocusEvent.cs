136caf9a1d7a363ed6a0f6c112ab96ef594a83d4
Li <limingyialpha@gmail.com>
2019-12-01 16:35:40 +0100

    Add class WindowFocusEvent to specity a window focus event

---
+++ application/Modules/WindowManagement/Events/WindowFocusEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace MORR.Modules.Mouse.Events
+{
+    /// <summary>
+    ///     A window foucs user interaction
+    /// </summary>
+    public class WindowFocusEvent : WindowEvent
+    {
+    }
+}
