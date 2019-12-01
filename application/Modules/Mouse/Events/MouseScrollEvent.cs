cec9bb1e10e2b21ac858d3be9545ee3603cc488b
Li <limingyialpha@gmail.com>
2019-12-01 11:58:56 +0100

    Add MouseScrollEvent

---
+++ application/Modules/Mouse/Events/MouseScrollEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MORR.Modules.Mouse.Events
+{
+    /// <summary>
+    ///     A mouse scroll user interaction
+    /// </summary>
+    public class MouseScrollEvent : MouseEvent
+    {
+        /// <summary>
+        ///     The amount of the wheel being scrolled
+        /// </summary>
+        public short ScrollAmount { get; set; }
+
+        /// <summary>
+        ///     The handle of the window in which the mouse scroll occurred
+        /// </summary>
+        public IntPtr HWnd { get; set; }
+    }
+}
