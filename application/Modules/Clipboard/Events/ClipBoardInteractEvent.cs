fcb2ebb61ec62eebc8f6feebdd9aba98b91f4384
Li <limingyialpha@gmail.com>
2019-12-01 12:36:48 +0100

    Add ClipboardInteractEvent to specify the clipboard interation event

---
+++ application/Modules/Clipboard/Events/ClipBoardInteractEvent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MORR.Modules.Clipboard.Events
+{
+    /// <summary>
+    ///     A clipboard user interaction
+    /// </summary>
+    public class ClipBoardInteractEvent : ClipboardEvent
+    {
+        /// <summary>
+        ///     An Enum to specify the four interaction type with a clip board:
+        ///     clear, copy, cut and paste
+        /// </summary>
+        public enum InteractionType
+        {
+            CLEAR, COPY, CUT, PASTE
+        };
+
+        /// <summary>
+        ///     The interaction type
+        /// </summary>
+        public InteractionType IAType { get; set;}
+
+        /// <summary>
+        ///     The text in the clipboard
+        /// </summary>
+        public String text { get; set; }
+    }
+}
