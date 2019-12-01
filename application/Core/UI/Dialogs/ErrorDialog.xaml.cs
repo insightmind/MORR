ef077f88bb125d379438dcd1199b5dc218d49408
Sönke <uynfo@student.kit.edu>
2019-12-01 16:12:03 +0100

    Added dialogs and rough styling

---
+++ application/Core/UI/Dialogs/ErrorDialog.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Morr.Core.UI.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for ErrorDialog.xaml
+    /// </summary>
+    public partial class ErrorDialog : Window
+    {
+        public ErrorDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ application/Core/UI/Dialogs/SaveDialog.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Morr.Core.UI.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for SaveDialog.xaml
+    /// </summary>
+    public partial class SaveDialog : Window
+    {
+        public SaveDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void OnAccept(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
