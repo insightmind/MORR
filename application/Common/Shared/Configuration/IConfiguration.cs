754fd7c882e6813a893ec80d154dc4164f4ababe
Sönke <uynfo@student.kit.edu>
2019-12-03 16:15:58 +0100

    Add configuration

---
+++ application/Common/Shared/Configuration/IConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace MORR.Shared.Configuration
+{
+    /// <summary>
+    ///     A self-contained unit of configuration
+    /// </summary>
+    public interface IConfiguration
+    {
+        /// <summary>
+        ///     Parses the configuration from the provided value
+        /// </summary>
+        /// <param name="configuration">The configuration <see cref="string" /> to parse from</param>
+        void Parse(string configuration);
+    }
+}
