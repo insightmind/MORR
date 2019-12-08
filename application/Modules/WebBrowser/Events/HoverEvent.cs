5be2be8e69800d3872a2dcaa981eb4c26eef3daa
irmasuppes <irma.suppes@yandex.ru>
2019-12-08 10:53:38 +0100

    Add inheritance from WebBrowserEvent in HoverEvent class

--- application/Modules/WebBrowser/Events/HoverEvent.cs
+++ application/Modules/WebBrowser/Events/HoverEvent.cs
@@ -8,7 +8,7 @@ namespace MORR.Modules.WebBrowser.Events
     /// <summary>
     /// A hover user interaction
     /// </summary>
-    public class HoverEvent
+    public class HoverEvent : WebBrowserEvent
     {
         /// <summary>
         /// The element on the website that has been hovered
