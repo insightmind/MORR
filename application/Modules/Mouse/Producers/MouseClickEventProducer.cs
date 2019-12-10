31fff0c15a619f3bcbd131333500eb034521723a
irmasuppes <irma.suppes@yandex.ru>
2019-12-10 11:52:18 +0100

    Add producers for mouse events

---
+++ application/Modules/Mouse/Producers/MouseClickEventProducer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MORR.Shared.Events.Queue;
+using MORR.Modules.Mouse.Events;
+using MORR.Shared.Events;
+using System.Composition;
+
+namespace MORR.Modules.Mouse.Producers
+{
+    /// <summary>
+    ///     Provides a single-writer-multiple-reader queue for MouseClickEvent
+    /// </summary>
+    [Export(typeof(MouseClickEventProducer))]
+    [Export(typeof(EventQueue<MouseClickEvent>))]
+    [Export(typeof(EventQueue<Event>))]
+    public class MouseClickEventProducer : EventQueue<MouseClickEvent>
+    {
+        /// <summary>
+        ///     Asynchronously gets all mouse click events as MouseClickEvent type
+        /// </summary>
+        /// <returns>A stream of MouseClickEvent</returns>
+        public override IAsyncEnumerable<MouseClickEvent> GetEvents()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        ///     Asynchronously enqueues a new mouse click event
+        /// </summary>
+        /// <param name="event">The mouse click event to enqueue</param>
+        protected override void Enqueue(MouseClickEvent @event)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ application/Modules/Mouse/Producers/MouseScrollEventProducer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MORR.Shared.Events.Queue;
+using MORR.Modules.Mouse.Events;
+using MORR.Shared.Events;
+using System.Composition;
+
+namespace MORR.Modules.Mouse.Producers
+{
+    /// <summary>
+    ///     Provides a single-writer-multiple-reader queue for MouseScrollEvent
+    /// </summary>
+    [Export(typeof(MouseScrollEventProducer))]
+    [Export(typeof(EventQueue<MouseScrollEvent>))]
+    [Export(typeof(EventQueue<Event>))]
+    public class MouseScrollEventProducer : EventQueue<MouseScrollEvent>
+    {
+        /// <summary>
+        ///     Asynchronously gets all mouse scroll events as MouseScrollEvent type
+        /// </summary>
+        /// <returns>A stream of MouseScrollEvent</returns>
+        public override IAsyncEnumerable<MouseScrollEvent> GetEvents()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        ///     Asynchronously enqueues a new mouse scroll event
+        /// </summary>
+        /// <param name="event">The mouse scroll event to enqueue</param>
+        protected override void Enqueue(MouseScrollEvent @event)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
