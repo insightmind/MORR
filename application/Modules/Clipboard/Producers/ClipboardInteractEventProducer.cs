b263ef29dbf399198b4c126b8c3607787c58493f
irmasuppes <irma.suppes@yandex.ru>
2019-12-10 12:13:43 +0100

    Add producers for clipboard and keyboard events

---
+++ application/Modules/Clipboard/Producers/ClipboardInteractEventProducer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MORR.Shared.Events.Queue;
+using MORR.Modules.Clipboard.Events;
+using MORR.Shared.Events;
+using System.Composition;
+
+namespace MORR.Modules.Clipboard.Producers
+{
+    /// <summary>
+    ///     Provides a single-writer-multiple-reader queue for ClipboardInteractEvent
+    /// </summary>
+    [Export(typeof(ClipboardInteractEventProducer))]
+    [Export(typeof(EventQueue<ClipboardInteractEvent>))]
+    [Export(typeof(EventQueue<Event>))]
+    public class ClipboardInteractEventProducer : EventQueue<ClipboardInteractEvent>
+    {
+        /// <summary>
+        ///     Asynchronously gets all clipboard interaction events as ClipboardInteractEvent type
+        /// </summary>
+        /// <returns>A stream of ClipboardInteractEvent</returns>
+        public override IAsyncEnumerable<ClipboardInteractEvent> GetEvents()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        ///     Asynchronously enqueues a new clipboard interaction event
+        /// </summary>
+        /// <param name="event">The clipboard interaction event to enqueue</param>
+        protected override void Enqueue(ClipboardInteractEvent @event)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ application/Modules/Keyboard/Producers/KeyboardInteractEventProducer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MORR.Shared.Events.Queue;
+using MORR.Modules.Keyboard.Events;
+using MORR.Shared.Events;
+using System.Composition;
+
+namespace MORR.Modules.Keyboard.Producers
+{
+    /// <summary>
+    ///     Provides a single-writer-multiple-reader queue for KeyboardInteractEvent
+    /// </summary>
+    [Export(typeof(KeyboardInteractEventProducer))]
+    [Export(typeof(EventQueue<KeyboardInteractEvent>))]
+    [Export(typeof(EventQueue<Event>))]
+    public class KeyboardInteractEventProducer : EventQueue<KeyboardInteractEvent>
+    {
+        /// <summary>
+        ///     Asynchronously gets all keyboard interaction events as KeyboardInteractEvent type
+        /// </summary>
+        /// <returns>A stream of KeyboardInteractEvent</returns>
+        public override IAsyncEnumerable<KeyboardInteractEvent> GetEvents()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        ///     Asynchronously enqueues a new keyboard interaction event
+        /// </summary>
+        /// <param name="event">The keyboard interaction event to enqueue</param>
+        protected override void Enqueue(KeyboardInteractEvent @event)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
