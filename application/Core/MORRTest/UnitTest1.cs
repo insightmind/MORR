54e344a88a50db8e728d2ee8e1a8177f9681cc18
Niklas Buelow <urpdf@student.kit.edu>
2020-01-12 17:02:31 +0100

    Add test prjects for dotnetCore projects

---
+++ application/Core/MORRTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MORRTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
