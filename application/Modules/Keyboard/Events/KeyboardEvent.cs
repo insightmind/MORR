b33deb8e14672d8e75070b183854d0c43a0165c1
Li <limingyialpha@gmail.com>
2020-01-19 17:14:23 +0100

    Specify the reason why KeyboardEvent is empty

--- application/Modules/Keyboard/Events/KeyboardEvent.cs
+++ application/Modules/Keyboard/Events/KeyboardEvent.cs
@@ -4,6 +4,7 @@ namespace MORR.Modules.Keyboard.Events
 {
     /// <summary>
     ///     A generic keyboard event which all specific KeyboardEvents inherit from.
+    ///     This class is empty because it is used only for restricting type usage.
     /// </summary>
     public abstract class KeyboardEvent : Event { }
 }
