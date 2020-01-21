9bbc5853b8b7a34073cf962487c6dab91a68bc6c
Sönke <uynfo@student.kit.edu>
2020-01-21 17:58:10 +0100

    Implement configuration parsing

--- application/Core/MORR/Recording/RecordingConfiguration.cs
+++ application/Core/MORR/Recording/RecordingConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Composition;
+using System.Text.Json;
 using MORR.Shared.Configuration;
 
 namespace MORR.Core.Recording
@@ -8,13 +9,23 @@ namespace MORR.Core.Recording
     [Export(typeof(IConfiguration))]
     public class RecordingConfiguration : IConfiguration
     {
+        /// <summary>
+        ///     The type of the encoder to use.
+        /// </summary>
         public Type Encoder { get; private set; }
 
+        /// <summary>
+        ///     The type of the decoder to use.
+        /// </summary>
         public Type Decoder { get; private set; }
+
+        public string Identifier => "Recording";
 
         public void Parse(string configuration)
         {
-            throw new System.NotImplementedException();
+            var instance = JsonSerializer.Deserialize<RecordingConfiguration>(configuration);
+            Encoder = instance.Encoder;
+            Decoder = instance.Decoder;
         }
     }
 }
