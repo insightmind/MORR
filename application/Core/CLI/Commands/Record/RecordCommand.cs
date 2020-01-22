c0daacc312d943bf6950520f246e4b37fb41e76c
Niklas Buelow <urpdf@student.kit.edu>
2020-01-22 23:01:43 +0100

    Add stubs for record command

---
+++ application/Core/CLI/Commands/Record/RecordCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Morr.Core.CLI.Commands.Record
+{
+    public class RecordCommand : ICLICommand<RecordOptions>
+    {
+        public int Execute(RecordOptions options)
+        {
+            // TODO: Implement this
+            return -1;
+        }
+    }
+}
---
+++ application/Core/CLI/Commands/Record/RecordOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommandLine;
+
+namespace Morr.Core.CLI.Commands.Record
+{
+    [Verb("record", HelpText = "Starts a new recording")]
+    public class RecordOptions : ICommandOptions
+    {
+        // TODO: Add parameters such as debug and recording file location and run config location
+    }
+}
