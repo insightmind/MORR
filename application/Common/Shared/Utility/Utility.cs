7ddbd7b488b234dea7ceeffb928018cf5611a67f
Sönke <uynfo@student.kit.edu>
2020-01-22 21:12:01 +0100

    Fix typo in Utility class

--- application/Common/Shared/Utility/Utility.cs
+++ application/Common/Shared/Utility/Utility.cs
@@ -11,11 +11,11 @@ namespace MORR.Shared.Utility
         /// <param name="value">The value to set the property to.</param>
         /// <param name="onTrue">
         ///     The action to execute if <paramref name="variable" /> was not set to <see langword="true" />
-        ///     before calling this method, but <paramref name="value" /> is <see langword="true" />
+        ///     before calling this method, but <paramref name="value" /> is <see langword="true" />.
         /// </param>
         /// <param name="onFalse">
-        ///     The action to execute if <paramref name="variable" /> was not set to <see langword="true" />
-        ///     before calling this method, but <paramref name="value" /> is <see langword="true" />
+        ///     The action to execute if <paramref name="variable" /> was not set to <see langword="false" />
+        ///     before calling this method, but <paramref name="value" /> is <see langword="false" />.
         /// </param>
         public static void SetAndDispatch(ref bool variable, bool value, Action onTrue, Action onFalse)
         {
