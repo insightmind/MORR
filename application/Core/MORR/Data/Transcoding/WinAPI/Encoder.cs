7c177cd28d80679be644eab06d04aee1a370af37
Sönke <uynfo@student.kit.edu>
2020-01-23 17:03:09 +0100

    Stub WinAPI encoder/decoder

--- application/Core/MORR/Data/Transcoding/WinAPI/Encoder.cs
+++ application/Core/MORR/Data/Transcoding/WinAPI/Encoder.cs
@@ -1,6 +1,4 @@
-﻿using MORR.Core.Data.Transcoding.Audio.EventHandlers;
-using MORR.Core.Data.Transcoding.Metadata.EventHandlers;
-using MORR.Core.Data.Transcoding.Video.EventHandlers;
+﻿using System;
 
 namespace MORR.Core.Data.Transcoding.WinAPI
 {
@@ -9,10 +7,9 @@ namespace MORR.Core.Data.Transcoding.WinAPI
     /// </summary>
     public class Encoder : IEncoder
     {
-        public bool IsEncoding { get; set; }
-        public string EncodingPath { get; set; }
-        public event AudioSampleRequestedEventHandler AudioSampleRequested;
-        public event VideoSampleRequestedEventHandler VideoSampleRequested;
-        public event MetadataSampleRequestedEventHandler MetadataSampleRequested;
+        public void Encode()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
