32623a9c453b6996605094a63b89563404d48ac9
Sönke <uynfo@student.kit.edu>
2020-01-24 12:57:32 +0100

    Fix dynamic assembly loading for non-standard dependencies

--- application/Core/MORR/Bootstrapper.cs
+++ application/Core/MORR/Bootstrapper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Composition;
 using System.Composition.Hosting;
 using System.IO;
@@ -37,15 +38,43 @@ namespace MORR.Core
 
         private void LoadFromPath(DirectoryPath path)
         {
-            var assemblies = Directory.GetFiles(path.ToString(), moduleNamePattern)
-                                      .Select(AssemblyLoadContext.Default.LoadFromAssemblyPath);
+            var moduleAssemblies = Directory.GetFiles(path.ToString(), moduleNamePattern)
+                                            .Select(ModuleLoadContext.Current.LoadModule);
 
             var containerConfiguration = new ContainerConfiguration();
-            containerConfiguration.WithAssemblies(assemblies)
+            containerConfiguration.WithAssemblies(moduleAssemblies)
                                   .WithAssembly(Assembly.GetExecutingAssembly())
                                   .WithAssembly(Assembly.GetEntryAssembly());
 
             container = containerConfiguration.CreateContainer();
+        }
+
+        private class ModuleLoadContext : AssemblyLoadContext
+        {
+            private readonly List<AssemblyDependencyResolver> resolvers = new List<AssemblyDependencyResolver>();
+
+            public static ModuleLoadContext Current { get; } = new ModuleLoadContext();
+
+            public Assembly LoadModule(string modulePath)
+            {
+                var resolver = new AssemblyDependencyResolver(modulePath);
+                resolvers.Add(resolver);
+
+                Default.LoadFromAssemblyPath(
+                    modulePath); // TODO This is only to make Type.GetType work elsewhere, remove if possible
+
+                return LoadFromAssemblyPath(modulePath);
+            }
+
+            protected override Assembly Load(AssemblyName assemblyName)
+            {
+                var assemblyPath = resolvers.Select(x => x.ResolveAssemblyToPath(assemblyName))
+                                            .FirstOrDefault(x => x != null);
+
+                // If the path cannot be resolved, do not return anything
+                // TODO This appears to work fine, resolve only fails for library dependencies, but this is not a good solution
+                return assemblyPath != null ? LoadFromAssemblyPath(assemblyPath) : null;
+            }
         }
     }
 }
