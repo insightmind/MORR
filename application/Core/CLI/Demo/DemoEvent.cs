1dda068c029f0c3ed80312936ae8c24d39c03e5d
Niklas Buelow <urpdf@student.kit.edu>
2020-01-25 12:00:47 +0100

    Create DemoProducer

---
+++ application/Core/CLI/Demo/DemoEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MORR.Shared.Events;
+
+namespace MORR.Core.CLI.Demo
+{
+    public class DemoEvent : Event
+    {
+        public int Num;
+
+        public DemoEvent()
+        {
+            var random = new Random();
+            Num = random.Next(0, 100000);
+        }
+    }
+}
---
+++ application/Core/CLI/Demo/DemoProducer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Timers;
+using MORR.Shared.Events.Queue;
+using MORR.Shared.Modules;
+
+namespace MORR.Core.CLI.Demo
+{
+    [Export(typeof(IModule))]
+    [Export(typeof(IReadOnlyEventQueue<DemoEvent>))]
+    public class DemoProducer : BoundedMultiConsumerEventQueue<DemoEvent>, IModule
+    {
+        private Timer timer;
+
+        public bool IsActive
+        {
+            get => timer.Enabled;
+            set => timer.Enabled = value;
+        }
+
+        public Guid Identifier => new Guid("9fff4e22-5815-4184-bf9c-e718e0f256a6");
+
+        public void Initialize()
+        {
+            timer = new Timer(100);
+            timer.AutoReset = true;
+            timer.Enabled = false;
+            timer.Elapsed += GenerateEvent;
+        }
+
+        private void GenerateEvent(Object source, ElapsedEventArgs args)
+        {
+            Enqueue(new DemoEvent());
+        }
+
+    }
+}
