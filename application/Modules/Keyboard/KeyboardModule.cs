9408b304fd0cbe3c3c97bef4e890a7fa20af14fb
Sönke <uynfo@student.kit.edu>
2020-01-25 13:37:17 +0100

    Refactor KeyboardModule and extract Win32-imports to Shared

--- application/Modules/Keyboard/KeyboardModule.cs
+++ application/Modules/Keyboard/KeyboardModule.cs
@@ -7,36 +7,32 @@ using MORR.Shared.Utility;
 namespace MORR.Modules.Keyboard
 {
     /// <summary>
-    ///     The <see cref="KeyboardModule" /> is responsible for recording all keyboard related user interactions
+    ///     The <see cref="KeyboardModule" /> is responsible for recording all keyboard related user interactions.
     /// </summary>
     [Export(typeof(IModule))]
     public class KeyboardModule : ICollectingModule
     {
         private bool isActive;
 
-        /// <summary>
-        ///     A single-writer-multiple-reader queue for KeyboardInteractEvent
-        /// </summary>
         [Import]
         private KeyboardInteractEventProducer KeyboardInteractEventProducer { get; set; }
 
         /// <summary>
-        ///     if the module is enabled or not.
-        ///     When a module is being enabled, the keyboard hook will be set.
-        ///     When a module is being disabled, the keyboard hook will be released.
+        ///     Indicates whether the module is active or not.
+        ///     When a module is being active, the keyboard hook will be set.
+        ///     When a module is being inactive, the keyboard hook will be released.
         /// </summary>
         public bool IsActive
         {
             get => isActive;
-            set => Utility.SetAndDispatch(ref isActive, value, KeyboardInteractEventProducer.HookKeyboard,
-                                          KeyboardInteractEventProducer.UnhookKeyboard);
+            set => Utility.SetAndDispatch(ref isActive, value, KeyboardInteractEventProducer.StartCapture,
+                                          KeyboardInteractEventProducer.StopCapture);
         }
 
-        public Guid Identifier => new Guid("99F679D6-0D20-40EE-8604-F128F0E5AE3B");
+        public static Guid Identifier = new Guid("99F679D6-0D20-40EE-8604-F128F0E5AE3B");
 
-        /// <summary>
-        ///     Initialize the module unenabled with KeyboardInteractEventProducer.
-        /// </summary>
+        Guid IModule.Identifier => Identifier;
+
         public void Initialize() { }
     }
 }
