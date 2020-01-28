3288a80748a73b1114d861b46103416f2b39de82
Sönke <uynfo@student.kit.edu>
2020-01-28 18:06:51 +0100

    Add IEncodeableEventQueue, IDecodeableEventQueue

---
+++ application/Common/Shared/Events/Queue/IDecodeableEventQueue.cs
@@ -0,0 +1,15 @@
+﻿namespace MORR.Shared.Events.Queue
+{
+    /// <summary>
+    ///     Provides a write-only queue for <see cref="Event" /> types intended for decoding.
+    /// </summary>
+    /// <typeparam name="T">The type of the event</typeparam>
+    public interface IDecodeableEventQueue<in T> where T : Event
+    {
+        /// <summary>
+        ///     Asynchronously enqueues a new event.
+        /// </summary>
+        /// <param name="event">The event to enqueue</param>
+        void Enqueue(T @event);
+    }
+}
--- application/Common/Shared/Events/Queue/ITranscodeableEventQueue.cs
+++ application/Common/Shared/Events/Queue/ITranscodeableEventQueue.cs
@@ -1,29 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace MORR.Shared.Events.Queue
+﻿namespace MORR.Shared.Events.Queue
 {
     /// <summary>
-    ///     Provides a single-writer-multiple-reader queue for <see cref="Event" /> types.
+    ///     Provides a single-writer-multiple-reader queue for <see cref="Event" /> types intended for transcoding.
     /// </summary>
     /// <typeparam name="T">The type of the event</typeparam>
-    public interface ITranscodeableEventQueue<T> where T : Event
-    {
-        /// <summary>
-        ///     Asynchronously gets all events as concrete type <typeparamref name="T" />.
-        /// </summary>
-        /// <returns>A stream of <typeparamref name="T" /></returns>
-        IAsyncEnumerable<T> GetEvents();
-
-        /// <summary>
-        ///     The actual type of the events enqueued in this queue.
-        /// </summary>
-        Type EventType => typeof(T);
-
-        /// <summary>
-        ///     Asynchronously enqueues a new event.
-        /// </summary>
-        /// <param name="event">The event to enqueue</param>
-        void Enqueue(T @event);
-    }
+    public interface ITranscodeableEventQueue<T> : IEncodeableEventQueue<T>, IDecodeableEventQueue<T>
+        where T : Event { }
 }
