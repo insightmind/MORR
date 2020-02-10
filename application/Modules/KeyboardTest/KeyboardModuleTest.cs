20e57a4fce7339e1d5a427b559f0c2503ffeb80f
Li <limingyialpha@gmail.com>
2020-02-10 22:19:39 +0100

    Initialize KeyboardModuleTest

---
+++ application/Modules/KeyboardTest/KeyboardModuleTest.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MORR.Modules.Keyboard;
+using MORR.Modules.Keyboard.Producers;
+
+namespace KeyboardTest
+{
+    [TestClass]
+    public class KeyboardModuleTest
+    {
+        private KeyboardModule keyboardModule;
+
+        [TestInitialize]
+        public void BeforeTest()
+        {
+            keyboardModule = new KeyboardModule();
+        }
+
+        [TestMethod]
+        public void TestKeyboardModule_OnActivate()
+        {
+            // Preconditions
+            Debug.Assert(keyboardModule != null);
+
+            /* GIVEN */
+            var keyboardInteractEventProducerMock = new Mock<KeyboardInteractEventProducer>();
+            var container = new CompositionContainer();
+            container.ComposeExportedValue(keyboardInteractEventProducerMock.Object);
+            container.ComposeParts(keyboardModule);
+
+            keyboardInteractEventProducerMock.Setup(mock => mock.StartCapture());
+
+            /* WHEN */
+            keyboardModule.IsActive = true;
+
+            /* THEN */
+            Assert.IsTrue(keyboardModule.IsActive);
+            keyboardInteractEventProducerMock.Verify(mock => mock.StartCapture(), Times.Once());
+        }
+    }
+}
