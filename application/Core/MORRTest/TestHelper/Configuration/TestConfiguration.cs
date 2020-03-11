1d917861158ea1881c0b80740d4f77dda33d836d
Niklas Buelow <urpdf@student.kit.edu>
2020-03-11 11:53:07 +0100

    Add TestConfiguration testhelper class

---
+++ application/Core/MORRTest/TestHelper/Configuration/TestConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using MORR.Shared.Configuration;
+using SharedTest.TestHelpers.Result;
+using System;
+
+namespace MORRTest.TestHelper.Configuration
+{
+    class TestConfiguration : IConfiguration
+    {
+        public TestResult testResult = new TestResult();
+        private readonly RawConfiguration expectedConfiguration;
+
+        public TestConfiguration(RawConfiguration expectedConfiguration)
+        {
+            this.expectedConfiguration = expectedConfiguration;
+        }
+
+        public void Parse(RawConfiguration configuration)
+        {
+            if (expectedConfiguration.RawValue.Equals(configuration.RawValue))
+            {
+                testResult.Complete();
+            } 
+            else
+            {
+                testResult.Fail(new ArgumentException());
+            }
+        }
+    }
+}
