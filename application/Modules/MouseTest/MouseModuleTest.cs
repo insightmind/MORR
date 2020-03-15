e43da70e4131ab9cd9b957a01ea999b4c288edbe
Li <limingyialpha@gmail.com>
2020-03-15 22:20:07 +0100

    Finish the test on MouseClickEventProducer
    Clean up the code and add some helper methods.

--- application/Modules/MouseTest/MouseModuleTest.cs
+++ application/Modules/MouseTest/MouseModuleTest.cs
@@ -13,6 +13,7 @@ using MORR.Modules.Mouse.Producers;
 using MORR.Shared.Events.Queue;
 using MORR.Shared.Hook;
 using SharedTest.TestHelpers.INativeHook;
+using System.Windows;
 
 namespace MouseTest
 {
@@ -20,6 +21,7 @@ namespace MouseTest
     public class MouseModuleTest
     {
         protected const int maxWaitTime = 5000;
+
         private class TestMouseModuleConfiguration : MouseModuleConfiguration
         {
             public TestMouseModuleConfiguration()
@@ -57,21 +59,17 @@ namespace MouseTest
             GlobalHook.MessageType.WM_MOUSEWHEEL
         };
 
-        private readonly GlobalHook.MessageType[] mouseMoveListenedMessagesTypes =
-        {
-            GlobalHook.MessageType.WM_MOUSEWHEEL
-        };
-
-
         [TestInitialize]
         public void BeforeTest()
         {
+            //initialize module, producers and configuration
             mouseModule = new MouseModule();
             mouseClickEventProducer = new MouseClickEventProducer();
             mouseMoveEventProducer = new MouseMoveEventProducer();
             mouseScrollEventProducer = new MouseScrollEventProducer();
             mouseModuleConfiguration = new TestMouseModuleConfiguration();
 
+            // initialize the container and fulfill the MEF inports exports
             container = new CompositionContainer();
             container.ComposeExportedValue(mouseClickEventProducer);
             container.ComposeExportedValue(mouseMoveEventProducer);
@@ -79,12 +77,13 @@ namespace MouseTest
             container.ComposeExportedValue(mouseModuleConfiguration);
             container.ComposeParts(mouseModule);
 
+            //initialzie the hookNativeMethodsMock
             hookNativeMethodsMock = new HookNativeMethodsMock();
             hookNativeMethodsMock.Initialize();
         }
 
         [TestMethod]
-        public void ModuleActivateTrue_ModuleActivated()
+        public void TestMouseModule_ActivateTrue()
         {
             // Preconditions
             Debug.Assert(mouseModule != null);
@@ -93,14 +92,7 @@ namespace MouseTest
 
             /* WHEN */
             mouseModule.Initialize(true);
-            foreach (GlobalHook.MessageType messageType in mouseClickListenedMessagesTypes)
-            {
-                hookNativeMethodsMock.AllowMessageTypeRegistry(messageType);
-            }
-            foreach (GlobalHook.MessageType messageType in mouseScrollListenedMessagesTypes)
-            {
-                hookNativeMethodsMock.AllowMessageTypeRegistry(messageType);
-            }
+            AllowMessageTypeRegistryForAll();
             hookNativeMethodsMock.AllowLibraryLoad();
             mouseModule.IsActive = true;
 
@@ -109,7 +101,7 @@ namespace MouseTest
         }
 
         [TestMethod]
-        public void ModuleActivateFalse_ModuleDeactivated()
+        public void TestMouseModule_ActivateFalse()
         {
             // Preconditions
             Debug.Assert(mouseModule != null);
@@ -125,7 +117,7 @@ namespace MouseTest
         }
 
         [TestMethod]
-        public async Task ModuleInitializeFalse_ChannelClosed()
+        public void TestMouseModule_InitializeFalse()
         {
             // Preconditions
             Debug.Assert(mouseModule != null);
@@ -141,7 +133,7 @@ namespace MouseTest
         }
 
         [TestMethod]
-        public async Task ModuleInitializeTrue_ChannelOpened()
+        public void TestMouseModuleInitializeTrue()
         {
             // Preconditions
             Debug.Assert(mouseModule != null);
@@ -158,7 +150,7 @@ namespace MouseTest
         }
 
         [TestMethod]
-        public void MouseClickProducerCallbackTest()
+        public void TestMouseClickEventProducer_Callback()
         {
             /* PRECONDITIONS */
             Debug.Assert(mouseModule != null);
@@ -167,21 +159,94 @@ namespace MouseTest
             Debug.Assert(hookNativeMethodsMock.Mock != null);
 
             /* GIVEN */
-            GlobalHook.CppGetMessageCallback callback = null;
+            GlobalHook.CppGetMessageCallback callback = GetCallback();
+            //setting up fake messages and corresponding expected Events
+            GlobalHook.HookMessage[] hookMessages = {
+            new GlobalHook.HookMessage { Type = (uint)GlobalHook.MessageType.WM_RBUTTONDOWN, Hwnd = (IntPtr)1, Data = new int[] { 1, 1 } },
+            new GlobalHook.HookMessage { Type = (uint)GlobalHook.MessageType.WM_LBUTTONDOWN, Hwnd = (IntPtr)2, Data = new int[] { 2, 2 } },
+            new GlobalHook.HookMessage { Type = (uint)GlobalHook.MessageType.WM_MBUTTONDOWN, Hwnd = (IntPtr)3, Data = new int[] { 3, 3 } },
+            new GlobalHook.HookMessage { Type = (uint)GlobalHook.MessageType.WM_RBUTTONDBLCLK, Hwnd = (IntPtr)4, Data = new int[] { 4, 4 } },
+            new GlobalHook.HookMessage { Type = (uint)GlobalHook.MessageType.WM_LBUTTONDBLCLK, Hwnd = (IntPtr)5, Data = new int[] { 5, 5 } },
+            new GlobalHook.HookMessage { Type = (uint)GlobalHook.MessageType.WM_MBUTTONDBLCLK, Hwnd = (IntPtr)6, Data = new int[] { 6, 6 } },
+            new GlobalHook.HookMessage { Type = (uint)GlobalHook.MessageType.WM_NCRBUTTONDOWN, Hwnd = (IntPtr)7, Data = new int[] { 7, 7 } },
+            new GlobalHook.HookMessage { Type = (uint)GlobalHook.MessageType.WM_NCLBUTTONDOWN, Hwnd = (IntPtr)8, Data = new int[] { 8, 8 } },
+            new GlobalHook.HookMessage { Type = (uint)GlobalHook.MessageType.WM_NCMBUTTONDOWN, Hwnd = (IntPtr)9, Data = new int[] { 9, 9 } },
+            new GlobalHook.HookMessage { Type = (uint)GlobalHook.MessageType.WM_NCRBUTTONDBLCLK, Hwnd = (IntPtr)10, Data = new int[] { 10, 10 } },
+            new GlobalHook.HookMessage { Type = (uint)GlobalHook.MessageType.WM_NCLBUTTONDBLCLK, Hwnd = (IntPtr)11, Data = new int[] { 11, 11 } },
+            new GlobalHook.HookMessage { Type = (uint)GlobalHook.MessageType.WM_NCMBUTTONDBLCLK, Hwnd = (IntPtr)12, Data = new int[] { 12, 12 } }
+            };
+            MouseClickEvent[] expectedEvents = {
+            new MouseClickEvent { MouseAction = MouseAction.RightClick, HWnd = "1", MousePosition = new Point { X = 1, Y = 1 } },
+            new MouseClickEvent { MouseAction = MouseAction.LeftClick, HWnd = "2", MousePosition = new Point { X = 2, Y = 2 } },
+            new MouseClickEvent { MouseAction = MouseAction.MiddleClick, HWnd = "3", MousePosition = new Point { X = 3, Y = 3 } },
+            new MouseClickEvent { MouseAction = MouseAction.RightDoubleClick, HWnd = "4", MousePosition = new Point { X = 4, Y = 4 } },
+            new MouseClickEvent { MouseAction = MouseAction.LeftDoubleClick, HWnd = "5", MousePosition = new Point { X = 5, Y = 5 } },
+            new MouseClickEvent { MouseAction = MouseAction.MiddleDoubleClick, HWnd = "6", MousePosition = new Point { X = 6, Y = 6 } },
+            new MouseClickEvent { MouseAction = MouseAction.RightClick, HWnd = "7", MousePosition = new Point { X = 7, Y = 7 } },
+            new MouseClickEvent { MouseAction = MouseAction.LeftClick, HWnd = "8", MousePosition = new Point { X = 8, Y = 8 } },
+            new MouseClickEvent { MouseAction = MouseAction.MiddleClick, HWnd = "9", MousePosition = new Point { X = 9, Y = 9 } },
+            new MouseClickEvent { MouseAction = MouseAction.RightDoubleClick, HWnd = "10", MousePosition = new Point { X = 10, Y = 10 } },
+            new MouseClickEvent { MouseAction = MouseAction.LeftDoubleClick, HWnd = "11", MousePosition = new Point { X = 11, Y = 11 } },
+            new MouseClickEvent { MouseAction = MouseAction.MiddleDoubleClick, HWnd = "12", MousePosition = new Point { X = 12, Y = 12 } }
+        };
 
+            /* WHEN */
+            //Running the task in another thread
+            var consumedEvent = new CountdownEvent(hookMessages.Length);
+            var task = new Task(() => findMatch(mouseClickEventProducer, consumedEvent, expectedEvents, isMouseClickEventFound));
+            task.Start();
+            // We must call the callback after we start the consumer for the producer.
+            // otherwise the message is automatically dismissed.
+            foreach(GlobalHook.HookMessage message in hookMessages) {
+                callback(message);
+            }
+
+            /* THEN */
+            Assert.IsTrue(consumedEvent.Wait(maxWaitTime), "Did not find all matching mouse events in time.");
+        }
+
+        /// <summary>
+        ///     See if all the events generated by the producer machtes the expectation
+        /// </summary>
+        /// <typeparam name="T">The type of event which will be captured.</typeparam>
+        /// <param name="producer">The producer which generates the events.</param>
+        /// <param name="countdown">A reset event to identify if all the predicate was met.param>
+        /// <param name="expectedEvents">an array of events that is expected to be generated by the producer</param>
+        /// <param name="predicate">A predicate which should return true if the expected event has been received.</param>
+        private async void findMatch<T>(DefaultEventQueue<T> producer, CountdownEvent countdown,T[] expectedEvents, Func<T,T[], bool> predicate) where T : MouseEvent
+        { 
+            await foreach (var @event in producer.GetEvents())
+            {
+                if(predicate.Invoke(@event,expectedEvents))
+                {
+                    countdown.Signal();
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Call AllowMessageTypeRegistry() methods on all messages related to the mouse producers.
+        /// </summary>
+        private void AllowMessageTypeRegistryForAll()
+        {
             foreach (GlobalHook.MessageType messageType in mouseClickListenedMessagesTypes)
             {
                 hookNativeMethodsMock.AllowMessageTypeRegistry(messageType);
             }
-
             foreach (GlobalHook.MessageType messageType in mouseScrollListenedMessagesTypes)
             {
                 hookNativeMethodsMock.AllowMessageTypeRegistry(messageType);
             }
+        }
 
+        /// <summary>
+        ///     Performs a series of initialization and Setups to get the CppGetMessageCallback.
+        /// </summary>
+        /// <returns>the callback that can be called with a message, which in turns calls a callback in the producers that is interested in this type of message</returns>
+        private GlobalHook.CppGetMessageCallback GetCallback() {
+            GlobalHook.CppGetMessageCallback callback= null;
+            AllowMessageTypeRegistryForAll();
             hookNativeMethodsMock.AllowLibraryLoad();
-
-            // !CHANGED! Renamed to improve readability
             var callbackReceivedEvent = new AutoResetEvent(false);
 
             hookNativeMethodsMock.Mock
@@ -191,8 +256,6 @@ namespace MouseTest
                      callback = cppCallback;
                      callbackReceivedEvent.Set();
                  });
-
-            /* WHEN */
             //here the SetHook() method is called!
             mouseModule.Initialize(true);
             mouseModule.IsActive = true;
@@ -200,48 +263,23 @@ namespace MouseTest
             //wait for the hookNativeMethodsMock.Mock.Callback is called!
             Assert.IsTrue(callbackReceivedEvent.WaitOne(maxWaitTime), "Did not receive callback in time!");
             Assert.IsNotNull(callback, "Callback received however unexpectedly null!");
-
-            //we can manipulate call back here
-
-            //setting up a fake message
-            int[] data = { 1, 1 };
-            GlobalHook.HookMessage hookMessage = new GlobalHook.HookMessage { Type = (uint)GlobalHook.MessageType.WM_RBUTTONDOWN, Hwnd = (IntPtr)1, Data = data };
-
-            // !CHANGED! Renamed to consumeEvent to improve readability
-            //Running the task in another thread
-            var consumedEvent = new ManualResetEvent(false);
-
-            // !CHANGED! We need to run the task in an async way. Therefore we do not use the await keyword
-            // otherwise this blocks.
-            var task = new Task(() => findMatch(mouseClickEventProducer, consumedEvent, (@event) => @event.MouseAction.Equals(MouseAction.RightClick)));
-            task.Start();
-
-            // !CHANGED! We must call the callback after we start the consumer for the producer.
-            // otherwise the message is automatically dismissed.
-            callback(hookMessage);
-
-            // !CHANGED! This failed beacuse you waited on the reset event for the CPPCallback receival
-            Assert.IsTrue(consumedEvent.WaitOne(maxWaitTime), "Did not find a matching mouse event in time.");
+            return callback;
         }
 
         /// <summary>
-        /// !CHANGED! I altered the method to allow any mouse producer.
-        /// It also uses a predicate to determine the event was received.
+        ///     Return true if an event is in the array of expected events.
+        ///     This method itslef defines the logic to determine if two events are equal.
         /// </summary>
-        /// <typeparam name="T">The type of event which will be captured.</typeparam>
-        /// <param name="producer">The producer which offers the events.</param>
-        /// <param name="reset">A reset event to identify if the predicate was met.</param>
-        /// <param name="predicate">A predicate which should return true if the expected event has been received.</param>
-        private async void findMatch<T>(DefaultEventQueue<T> producer, ManualResetEvent reset, Func<T, bool> predicate) where T : MouseEvent
-        { 
-            await foreach (var @event in producer.GetEvents())
+        /// <param name="event">an event that is expected to be in the array of expected events</param>
+        /// <param name="expectedEvents">an array of expected events</param>
+        /// <returns>true if an event is indeed in the array of expected events.</returns>
+        private bool isMouseClickEventFound(MouseClickEvent @event, MouseClickEvent[] expectedEvents)
+        {
+            foreach (MouseClickEvent e in expectedEvents)
             {
-                if(predicate.Invoke(@event))
-                {
-                    reset.Set();
-                    break;
-                }
+                if (@event.MouseAction.Equals(e.MouseAction) && @event.MousePosition.Equals(e.MousePosition) && @event.HWnd.Equals(e.HWnd)) return true;
             }
+            return false;
         }
     }
 }
