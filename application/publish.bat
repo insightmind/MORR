69faf5a3bfda48fd90bb51a60d3fb81b5f086a3d
Karl <ucpcg@student.kit.edu>
2020-03-23 21:00:41 +0100

    Second revision buildscript. Excluded testprojects.

--- application/publish.bat
+++ application/publish.bat
@@ -1,22 +1,13 @@
 @echo off
-set targetframework=netcoreapp3.1
-set outdir=publishing
-set moduleoutdir=publishing\Modules
-set noselfcontained=( ^
-Core\CLI\CLI.csproj ^
-Common\Shared\Shared.csproj)
-set coreprojects=( ^
-Core\MORR\MORR.csproj ^
-Core\UI\UI.csproj)
-set noselfcontainedmodules=( ^
-Modules\Mouse\Mouse.csproj ^
-Modules\WindowManagement\WindowManagement.csproj)
-set modules=( ^
-Modules\Clipboard\Clipboard.csproj ^
-Modules\Keyboard\Keyboard.csproj ^
-Modules\Webbrowser\WebBrowser.csproj)
-for %%i in %noselfcontained% do dotnet publish -o %outdir% -f %targetframework% -c release %%i
-for %%i in %coreprojects% do dotnet publish -o %outdir% -c release -r win10-x64 -f %targetframework% --self-contained true %%i
-for %%i in %modules% do dotnet publish -o %moduleoutdir% -c release -r win10-x64 -f %targetframework% --self-contained true %%i
-for %%i in %noselfcontainedmodules% do dotnet publish -o %moduleoutdir% -c release -f %targetframework% %%i
+set outdir=publish
+set "ModuleExt=MORR-Module.dll"
+dotnet clean
+dotnet publish -c release --no-dependencies -o %outdir% MORR.sln
+dotnet clean
+dotnet publish -c release --no-dependencies -o %outdir% -r win-x64 --self-contained true MORR.sln
+md %outdir%\Modules
+move %outdir%\*%ModuleExt% %outdir%\Modules\
+copy build\Release\x64\HookLibrary*.dll %outdir%\
+copy build\Release\x64\Win32HookHelper.exe %outdir%\
+copy BuildEnvironment\config.morr %outdir%\
 set /p asd="Hit enter to continue"
